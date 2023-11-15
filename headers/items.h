bd6a7cc61ae1d67ecfeaac4cb92e1a9a79a03eb7
SomeRandomGuy45 <143886716+SomeRandomGuy45@users.noreply.github.com>
2023-11-15 06:50:18 -0600

    Update items.h

--- headers/items.h
+++ headers/items.h
@@ -4,5 +4,12 @@ using namespace std;
 
 class Item
 {
-    
+public:
+    string Name;
+    int healthGiving = 100;
+    int defGiving = 10;
+    int magicGiving = 2;
+    bool GiveCustom = false;
+    string CustomGiving;
+    int GivingCustom = 0;
 };
