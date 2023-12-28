765b401364a07500f9aa1036313467c6b9bdfaee
SomeRandomGuy45 <143886716+SomeRandomGuy45@users.noreply.github.com>
2023-12-27 22:16:44 -0600

    very small update

--- headers/items.h
+++ headers/items.h
@@ -38,19 +38,39 @@ public:
     int GivingCustom = 0;
 };
 
+class Item2
+{
+public:
+    string Name = "NAME2";
+    int healthGiving = 150;
+    int defGiving = 15;
+    int magicGiving = 7;
+    bool GiveCustom = false;
+    string CustomGiving;
+    int GivingCustom = 0;
+};
+
 void CheckItems()
 {
     for (int i = 0; i < 5; i++)
     {
-        cout << CurrentItem[i] << "\n";
+        if (CurrentItem[i] == "")
+        {
+            std::cout << "\n";
+        }
+        else
+        {
+            cout << CurrentItem[i] << "\n";
+        }
     }
 }
 
 void AddItem()
 {
     //Default Item
+    //Insert Your item classes here!
     Item item1;
-    Item item2;
+    Item2 item2;
     //Insert your item here!
     string Items[] = {item1.Name, item2.Name};
     //How Many Items we have
@@ -66,4 +86,9 @@ void AddItem()
             break;
         }
     }
+}
+
+void RemoveItems(int slot)
+{
+    CurrentItem[slot] = "";
 }
